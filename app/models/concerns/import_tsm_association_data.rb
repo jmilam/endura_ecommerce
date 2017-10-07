59ef66873267608ee0fc71d8f27fb9e1a42caae9
Jason Milam <jasonlmilam@gmail.com>
2017-10-06 21:59:32 -0400

    Create user on TSM/Sales Rep import

--- app/models/concerns/import_tsm_association_data.rb
+++ app/models/concerns/import_tsm_association_data.rb
@@ -12,33 +12,57 @@ class ImportTsmAssociationData
 			  companies = false
 
 			  data.each do |row|
-				  if row[0].downcase == "tsm"
-					  tsm = true
-					  rep = false
-					  companies = false
-				  elsif row[0].downcase == "sales rep"
-					  tsm = false
-					  companies = false
-					  rep = true
-				  elsif row[0].downcase == "companies"
-					  rep = false
-					  tsm = false
-					  companies = true
-					elsif tsm
-					  Tsm.create(name: row[0], email: row[1])
-					elsif rep
-					  Tsm.last.sales_reps.create(name: row[0], email: row[1], rep_group: row[2])
-					elsif companies
-					  rep_group = SalesRep.last.rep_group
-					  unless row[0].downcase == "company name"
-						  customer = SalesRep.last.customers.create(company_name: row[0], contact_email: row[2], phone_number: row[4], address: row[5], city: row[6], state: row[7], zipcode: row[8].to_i, company_contact: row[1], rep_group: row[3])
-						  FundsBank.create(customer_id: customer.id, allocated_amt: row[9], current_bal: row[10])
-						 end
+			  	p row[0]
+			  	unless row[0].nil?
+					  if row[0].downcase == "tsm"
+						  tsm = true
+						  rep = false
+						  companies = false
+					  elsif row[0].downcase == "sales rep"
+						  tsm = false
+						  companies = false
+						  rep = true
+					  elsif row[0].downcase == "companies"
+						  rep = false
+						  tsm = false
+						  companies = true
+						elsif tsm
+						  Tsm.create(name: row[0], email: row[1])
+						  create_user(row[0], row[1], 'EnduraTSM1')
+						elsif rep
+						  Tsm.last.sales_reps.create(name: row[0], email: row[1], rep_group: row[2])
+						  create_user(row[0], row[1], 'Endura1234')
+						elsif companies
+						  rep_group = SalesRep.last.rep_group
+						  unless row[0].downcase == "company name"
+						  	p "CREATE CUSTOMER"
+							  customer = SalesRep.last.customers.create(company_name: row[0],
+							  																					contact_email: row[2],
+							  																					phone_number: row[4],
+							  																					address: row[5],
+							  																					city: row[6],
+							  																					state: row[7],
+							  																					zipcode: row[8].to_i,
+							  																					company_contact: row[1],
+							  																					rep_group: row[3])
+
+							  p customer.errors
+							  FundsBank.create(customer_id: customer.id, allocated_amt: row[9], current_bal: row[10])
+							 end
+				  	end
 				  end
 				end
 			rescue Exception => e
 			  p e
 			end
+		end
+	end
+
+	def create_user(name, email, password)
+		user = User.find_by_email(email)
+
+		unless user.nil?
+			User.create(name: name, email: email, password: password)
 		end
 	end
 end
